ef088e64caebcdad29398e86f060b529141c5e07
Evgen <evgenprogrammer@gmail.com>
2020-04-18 21:54:42 +0300

    Gorutina

--- gotest/main.go
+++ gotest/main.go
@@ -2,37 +2,18 @@ package main
 
 import "fmt"
 
-type Adult interface {
-	IsAdult() bool
-	fmt.Stringer
-}
-
-type Person struct {
-	age  int
-	name string
-}
-
-func (m Person) String() string {
-	return fmt.Sprintf("%s is %d", m.name, m.age)
-}
-
-func (m Person) IsAdult() bool {
-	return m.age >= 18
-}
-
-func adultFilter(people []Adult) []Adult {
-	adults := make([]Adult, 0)
-	for _, p := range people {
-		if p.IsAdult() {
-			adults = append(adults, p)
-		}
-	}
-	return adults
-}
-
 func main() {
-	people := []Adult{Person{15, "John"}, Person{18, "Joe"}, Person{45, "Mory"}}
-	fmt.Println(adultFilter(people))
-}
+	c := make(chan bool)
+	m := make(map[string]string)
 
-//Joe is 18
+	go func() {
+		m["1"] = "a"
+		c <- true
+	}()
+
+	m["2"] = "b"
+	<-c
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+}
