fbe662eff27e3cae63e2859abaf6420385df692b
Evgen <evgenprogrammer@gmail.com>
2020-04-21 19:13:56 +0300

    struct project

--- gotest/main.go
+++ gotest/main.go
@@ -1,18 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
-func main() {
-	arr := []int{1,2,3}
-	mp := map[string]string{"qwe":"asd"}
-	ch:= make(chan int,3)
-	var st struct{
-		s string
-		i int
+func main(){
+	f:=fmt.Printf
+	v:=reflect.ValueOf(f)
+	args:=[]reflect.Value{
+		reflect.ValueOf("test %d/n"),
+		reflect.ValueOf(42),
 	}
-
-	fmt.Printf("%p\n%v\n%#v\n\n",arr,arr,arr)
-	fmt.Printf("%p\n%v\n%#v\n\n",mp,mp,mp)
-	fmt.Printf("%p\n%v\n%#v\n\n",st,st,st)
-	fmt.Printf("%p\n%v\n%#v\n\n",ch,ch,ch)
+	ret:=v.Call(args) //[]reflect.Value
+	fmt.Println(ret)
 }
