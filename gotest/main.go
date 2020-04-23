954fad5cfc9f12f42098e924c1ce55faf7574f52
Evgen <evgenprogrammer@gmail.com>
2020-04-23 17:26:33 +0300

    :)

--- gotest/main.go
+++ gotest/main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+func main() {
 
-func main(){
-	f:=fmt.Printf
-	v:=reflect.ValueOf(f)
-	args:=[]reflect.Value{
-		reflect.ValueOf("test %d/n"),
-		reflect.ValueOf(42),
-	}
-	ret:=v.Call(args) //[]reflect.Value
-	fmt.Println(ret)
 }
