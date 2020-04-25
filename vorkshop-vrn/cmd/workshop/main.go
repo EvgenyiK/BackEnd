61e2bb9b1969e1e73b7115cbb9c1ed4aaa1f544b
Evgen <evgenprogrammer@gmail.com>
2020-04-25 12:01:40 +0300

    golang_workshop

--- vorkshop-vrn/cmd/workshop/main.go
+++ vorkshop-vrn/cmd/workshop/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"github.com/go-chi/chi"
+	"log"
+	"net/http"
+	"workshop_vrn/internal/handler"
+)
 
+func main() {
+	h := handler.NewHandler()
+	r := chi.NewRouter()
+	r.Get("/hello", h.Hello)
+	err := http.ListenAndServe(":8080", r)
+	log.Fatal(err)
 }
